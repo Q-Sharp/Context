13159f2426a253103446b24ab1a93c240d289961
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2023-07-17 01:53:41 +0200

    Add project files.

---
+++ Problems/ContextWith2Ctors.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Problem;
+
+public class TestContextWith2Ctors : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+             => options
+                 .EnableServiceProviderCaching(false)
+                 .UseInMemoryDatabase(nameof(TestContextWith2Ctors));
+
+    public TestContextWith2Ctors(DbContextOptions<TestContextWith2Ctors> options) : base(options)
+    {
+    }
+
+    public TestContextWith2Ctors()
+    {
+    }
+}
